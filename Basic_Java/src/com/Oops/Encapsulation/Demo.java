9c421dbbfa949d7cb3afca286b1a20420b25f51a
Aradhana Samantray <aradhanasamantray7@gmail.com>
2026-05-25 17:13:50 +0530

    Encapsulation

---
+++ Basic_Java/src/com/Oops/Encapsulation/Demo.java
@@ -0,0 +1,11 @@
+package com.Oops.Encapsulation;
+    public class Demo{
+        public static void main(String[] args){
+            Student s1 =new Student();
+            s1.setName("Aradhana");
+            s1.setId(117);
+            System.out.println("Student Name: " + s1.getName());
+            System.out.println("Student Id: " + s1.getId());
+        }
+    }
+
